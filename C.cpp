c87d3645608bfdc96c6d3f2f378f9d53d65b5dba
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-07-03 19:10:53 -0300

    Criacao da classe c

---
+++ C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class C{
+private:
+  std::string C1;
+  int C2;
+public:
+
+    void MC1() {
+    std::cout << "MC1\n";
+  }
+
+    void MC2() {
+    std::cout << "MC2\n";
+  }
+};
